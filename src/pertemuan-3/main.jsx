8832e22b7406112bc1ca2ea934fd55b6c5463144
nailah24si@mahasiswa.pcr.ac.id <naufal24si@mahasiswa.pcr.ac.id>
2026-03-30 15:50:58 +0700

    third commit

---
+++ src/pertemuan-3/main.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from "react-dom/client";
+import TailwindCSS from "./TailwindCSS";
+import "./tailwind.css";
+import Typography from "./Typography";
+import FlexboxGrid from "./FlexboxGrid";
+import InputField from "./InputField";
+import UserForm from "./UserForm";
+import HitungGajiForm from "./HitungGajiForm";
+
+createRoot(document.getElementById("root"))
+  .render(
+    <div>
+      {/* <TailwindCSS />
+      <Typography />
+      <FlexboxGrid />
+      <InputField/>
+      <UserForm/> */}
+      <HitungGajiForm/>
+    </div>
+  );
