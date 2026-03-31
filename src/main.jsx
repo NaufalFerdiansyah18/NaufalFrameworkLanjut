9ee8f1c9c82fa35337e6c0020993f11d3ebbcfbc
NaufalFerdiansyahPutra <naufalperdiansyah@gmail.com>
2026-03-31 21:53:01 +0700

    tugas3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './tugas-3/tailwind.css'
+import FormBudget from './tugas-3/FormBudget'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FormBudget />
   </StrictMode>,
 )
